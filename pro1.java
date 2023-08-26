d9ee9362b747e43c75fb3c527e2dd0c8ad4bd313
nup20 <130826435+nup20@users.noreply.github.com>
2023-08-26 19:00:09 +0530

    Add files via upload

---
+++ pro1.java
@@ -0,0 +1,38 @@
+package stringpractice;
+import java.util.Scanner;
+
+public class pro1 {
+int a=10;
+
+	public void add()
+	{
+		System.out.println(+a);
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s;
+		int count=0;
+		
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter string=");
+		s=sc.nextLine();
+		
+		
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)!=' ')
+			count++;
+				
+			
+		}
+		
+			
+		System.out.println("no of char are="+ count);
+	
+	}
+
+}
+
