7017b8ca6505d6920996fae911a8b99237818b57
nup20 <130826435+nup20@users.noreply.github.com>
2023-11-17 04:38:03 -0800

    Add files via upload

---
+++ Logical Programs/class3.java
@@ -0,0 +1,20 @@
+package New;
+
+public class class3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int a=2;
+         int b=3;
+         System.out.println("a is:"+a);
+ 		 System.out.println("b is:"+b);
+ 		 a=a+b;
+ 		 b=a-b;
+ 		 a=a-b;
+ 		System.out.println("After Swaping:");
+		System.out.println("a is:"+a);
+		System.out.println("b is:"+b);
+		
+	}
+
+}
